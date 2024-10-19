40cae08ef17bc9e5f105bafdf21f3cce742ce695
Skube77 <acilmajed5@gmail.com>
2024-10-19 21:55:37 +0100

    Delete src directory

--- src/main/java/com/ezlearning/platform/repositories/ProfesorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ezlearning.platform.repositories;
-
-import com.ezlearning.platform.model.Profesor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
-}
